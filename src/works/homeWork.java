fca1ccfa1fbed32aaf21b83b181d3827b94fae1e
vahag <MartinSahakyan1973@gmail.com>
2022-05-14 20:36:58 +0400

    my First Home Work

---
+++ src/works/homeWork.java
@@ -0,0 +1,29 @@
+package works;
+
+public class homeWork {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 6; i++) {
+            for (j = 6; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 6; j > i; j--) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+
+
+        }
+    }
+
+}
